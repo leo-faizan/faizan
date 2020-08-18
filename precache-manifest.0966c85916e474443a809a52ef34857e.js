4c9d09fefd73b7009e717acce2e3c332014a3f0a
faizan <faizanhamid7860@gmail.com>
2020-08-18 12:10:34 +0530

    Updates

---
+++ precache-manifest.0966c85916e474443a809a52ef34857e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0999a2bf881e29985cf76e2567414bdd",
+    "url": "/faizan/index.html"
+  },
+  {
+    "revision": "655ddf8208d1c091652e",
+    "url": "/faizan/static/css/main.ce5a91e6.chunk.css"
+  },
+  {
+    "revision": "ace89434a156504edd07",
+    "url": "/faizan/static/js/2.f7f9e211.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/faizan/static/js/2.f7f9e211.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "655ddf8208d1c091652e",
+    "url": "/faizan/static/js/main.876e2706.chunk.js"
+  },
+  {
+    "revision": "8786b6067e75e133bd24",
+    "url": "/faizan/static/js/runtime-main.30a8b810.js"
+  },
+  {
+    "revision": "6696d7e1503cfead57793d773e28505e",
+    "url": "/faizan/static/media/bgImg.6696d7e1.jpg"
+  },
+  {
+    "revision": "307871b8d90146a4cc2379f60f3c10dc",
+    "url": "/faizan/static/media/dark.307871b8.png"
+  },
+  {
+    "revision": "be8c920486b487a2a41cd2a28eea4716",
+    "url": "/faizan/static/media/logo.be8c9204.svg"
+  }
+]);
